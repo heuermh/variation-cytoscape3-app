61d65cc21d47ac15770d42b01e6e4dc9e63fb8aa
neurons <neurons@users.noreply.github.com>
2014-06-27 02:38:50 -0400

    Create AdamVariant.java

---
+++ variation/src/main/java/org/dishevelled/variation/adam/AdamVariant.java
@@ -0,0 +1,67 @@
+/*
+
+    dsh-variation  Variation.
+    Copyright (c) 2013-2014 held jointly by the individual authors.
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+
+public class AdamVariant{
+  	private AdamContig contig;
+  	private String position;
+  	private String exclusiveEnd;
+  	private String referenceAllele;
+  	private String variantAllele;
+
+  	public ADAMVariant(AdamContig contig, String position, String exclusiveEnd, String referenceAllele, String variantAllele)
+  	{
+  		this.contig = contig;
+  		this.position = position;
+  		this.exclusiveEnd = exclusiveEnd;
+  		this.referenceAllele = referenceAllele;
+  		this.variantAllele = variantAllele;
+  
+  	}
+  
+  	public void getContig()
+  	{
+  		return contig;
+  	}
+  	
+  	public void getPosition()
+  	{
+  		return position;
+  	}
+  
+  	public void getExclusiveEnd()
+  	{
+  		return exclusiveEnd;
+  	}
+  
+  	public void getReferenceAllele()
+  	{
+  		return referenceAllele;
+  	}
+  
+  	public void getVariantAllele()
+  	{
+  		return variantAllele;
+  	}
+
+}
